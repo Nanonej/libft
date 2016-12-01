bcb5aa271274b2a7ffc0ae820aec46cb5c933ee0
Antonin RIDOLFI <aridolfi@student.42.fr>
2016-12-01 15:35:01 +0100

    rename begin_list to alst in ft_lstback

--- ft_lstback.c
+++ ft_lstback.c
@@ -6,17 +6,17 @@
 /*   By: aridolfi <aridolfi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/11 23:49:55 by aridolfi          #+#    #+#             */
-/*   Updated: 2016/11/11 23:50:50 by aridolfi         ###   ########.fr       */
+/*   Updated: 2016/12/01 15:34:12 by aridolfi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstback(t_list **begin_list, t_list *new)
+void	ft_lstback(t_list **alst, t_list *new)
 {
 	t_list		*lst;
 
-	lst = *begin_list;
+	lst = *alst;
 	if (lst)
 	{
 		while (lst->next)
