de46f2b412f35f6656d2ce685047099b111080f8
Antonin RIDOLFI <aridolfi@student.42.fr>
2017-02-08 17:38:30 +0100

    fix leaks

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: aridolfi <aridolfi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/11 18:56:19 by aridolfi          #+#    #+#             */
-/*   Updated: 2016/11/11 22:03:24 by aridolfi         ###   ########.fr       */
+/*   Updated: 2017/02/07 22:47:10 by aridolfi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,12 @@ char	*ft_itoa(int n)
 
 	i = 0;
 	k = 1;
-	if (!(new = ft_strnew(ft_digitcount(n))))
-		return (NULL);
+	new = ft_strnew(ft_digitcount(n));
 	if (n == 0 || n == -2147483648)
+	{
+		free(new);
 		return (n == 0 ? ft_strdup("0") : ft_strdup("-2147483648"));
+	}
 	if (n < 0)
 	{
 		n = -n;
